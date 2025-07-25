a880ea226a4853011795130542119d0d80ed8326
Tim Ptochopoulos <timptochop@gmail.com>
2025-07-25 16:34:32 +0300

    Fix import path for LiveTennis.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import LiveTennis from './LiveTennis'; // ✅ Σωστό import
+import './App.css';
+import './index.css';
+import TopBar from './components/TopBar';
+import LiveTennis from './LiveTennis'; // <== ΔΙΟΡΘΩΜΕΝΟ path
 
 function App() {
   return (
     <div className="App">
+      <TopBar />
       <LiveTennis />
     </div>
   );
