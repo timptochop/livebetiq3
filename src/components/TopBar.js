1c09231fe2bc3e3f480e1f9a3c7c5fe8b1aed228
Tim Ptochopoulos <timptochop@gmail.com>
2025-07-30 20:11:09 +0300

    Shifted login icon 2x left for better spacing

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -37,7 +37,7 @@ function TopBar({ onLoginClick }) {
         alignItems: 'center',
         justifyContent: 'space-between'
       }}>
-        {/* Left: Logo */}
+        {/* Logo */}
         <div>
           <img
             src="/logo192.png"
@@ -46,12 +46,9 @@ function TopBar({ onLoginClick }) {
           />
         </div>
 
-        {/* Right: Settings + Time + Login */}
-        <div style={{ display: 'flex', alignItems: 'center', gap: '18px' }}>
-          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-            <FaCog color="#ccc" size={20} />
-            <span style={{ color: '#ccc', fontSize: '13px' }}>{currentTime}</span>
-          </div>
+        {/* Login + Time + Settings */}
+        <div style={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
+          {/* Login icon */}
           <div
             onClick={onLoginClick}
             style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}
@@ -66,6 +63,12 @@ function TopBar({ onLoginClick }) {
               <path d="M12 12c2.7 0 5-2.3 5-5s-2.3-5-5-5-5 2.3-5 5 2.3 5 5 5zm0 2c-3.3 0-10 1.7-10 5v3h20v-3c0-3.3-6.7-5-10-5z" />
             </svg>
           </div>
+
+          {/* Current time */}
+          <span style={{ color: '#ccc', fontSize: '13px' }}>{currentTime}</span>
+
+          {/* Settings icon */}
+          <FaCog color="#ccc" size={20} />
         </div>
       </div>
 
