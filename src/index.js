d2260344dd0298c329ed3f2bf0720cd28649c737
Tim Ptochopoulos <timptochop@gmail.com>
2025-09-27 08:54:34 +0300

    fix(index): overlay block, safe-area reset, styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
