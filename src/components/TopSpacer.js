8a72331359f93dac5744f4d21b261652e3b695d6
Tim Ptochopoulos <timptochop@gmail.com>
2025-09-27 08:23:49 +0300

    ui(topbar): opaque black + high z-index + spacer to prevent underlap

--- src/components/TopSpacer.js
+++ src/components/TopSpacer.js
@@ -1,16 +1,6 @@
-// src/components/TopSpacer.js
-import React from "react";
+import React from 'react';
+import './TopBar.css'; // για την .topbar-spacer
 
 export default function TopSpacer() {
-  return (
-    <div style={{
-      height: 40,
-      background: "#0a0c0e",
-      width: "100%",
-      position: "fixed",
-      top: 0,
-      left: 0,
-      zIndex: 999
-    }} />
-  );
+  return <div className="topbar-spacer" />;
 }
